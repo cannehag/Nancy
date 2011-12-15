fa332fb5d33c9651063603b0a6d561dd3ee6f757
Andreas Hakansson <andreas@selfinflicted.org>
2011-12-15 11:53:50 +0100

    Removed IRouteCache from IRouteResolver.Resolve
    The cache used to be passed in each time when a route was being
    resolved. The DefaultRouteResolver now takes a dependency on the
    IRouteCache instead.

--- src/Nancy/Bootstrapper/NancyBootstrapperBase.cs
+++ src/Nancy/Bootstrapper/NancyBootstrapperBase.cs
@@ -4,6 +4,7 @@
     using System.Collections.Generic;
     using System.Diagnostics.CodeAnalysis;
     using System.Linq;
+    using Diagnostics;
     using Nancy.Cryptography;
     using Nancy.ModelBinding;
     using Nancy.Conventions;
@@ -96,6 +97,7 @@
                     ?? 
                     (this.modules = AppDomainAssemblyTypeScanner
                                         .TypesOf<NancyModule>(true)
+                                        .NotOfType<DiagnosticModule>()
                                         .Select(t => new ModuleRegistration(t, this.GetModuleKeyGenerator().GetKeyForModuleType(t)))
                                         .ToArray());
             }
